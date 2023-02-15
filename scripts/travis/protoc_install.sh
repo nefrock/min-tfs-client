595cab869044fdeee004a84c8980ae643f6fba07
Peter Spalthoff <peter.spalthoff@gmail.com>
2023-02-15 11:55:25 +0900

    Bump protoc to TF 2.11.0 compatible version

--- scripts/travis/protoc_install.sh
+++ scripts/travis/protoc_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-PROTOBUF_VERSION=3.3.0
+PROTOBUF_VERSION=3.19.6
 PROTOC_FILENAME=protoc-${PROTOBUF_VERSION}-linux-x86_64.zip
 pushd ${HOME}
 wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/${PROTOC_FILENAME}
